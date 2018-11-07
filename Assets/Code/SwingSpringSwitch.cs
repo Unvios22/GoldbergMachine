90a8f18fb85f7ac09ee2f0911469c2f70432713a
Unvios22 <unvios22@gmail.com>
2018-11-08 00:25:35 +0100

    Added Swing

---
+++ Assets/Code/SwingSpringSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Code {
+	public class SwingSpringSwitch : MonoBehaviour {
+
+		[SerializeField] private HingeJoint _swingJoint;
+		[SerializeField] private float _waitTime = 1f;
+
+		private void Start() {
+			_swingJoint = this.GetComponentInChildren<HingeJoint>();
+		}
+
+		private void OnTriggerEnter(Collider other) {
+			_swingJoint.useSpring = false;
+		}
+
+		private void OnTriggerExit(Collider other) {
+			StartCoroutine(ReturnToStartingPosition());
+		}
+
+		IEnumerator ReturnToStartingPosition() {
+			yield return new WaitForSeconds(_waitTime);
+			_swingJoint.useSpring = true;
+		}
+	}
+}
