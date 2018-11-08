fc047854e0a516a500f854dd4cd6aeefe11d28fa
Unvios22 <unvios22@gmail.com>
2018-11-08 01:39:28 +0100

    Added Float Obstacles && changed project .NET version to 4.x

--- Assets/Code/ReturnToStartPosition.cs
+++ Assets/Code/ReturnToStartPosition.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
-public class ReturnToStartPosition : MonoBehaviour {
+namespace Code {
+	public class ReturnToStartPosition : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+		[SerializeField] private float _waitTime;
+		[SerializeField] private int _returnTime;
+		private Vector3 _startingPosition;
+		private Quaternion _startingRotation;
+		private Rigidbody _rigidbody;
+		private Transform _transform;
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		private void Start() {
+			_startingPosition = transform.position;
+			_startingRotation = transform.rotation;
+			_rigidbody = transform.GetComponent<Rigidbody>();
+			_transform = transform;
+		}
+
+		private void OnEnable() {
+			GameObject.FindWithTag("Trigger").GetComponent<ReturnToStartPositionTrigger>().TriggerEvent += OnTrigger;
+		}
+
+		private void OnDisable() {
+			ReturnToStartPositionTrigger trigger = GameObject.FindWithTag("Trigger").GetComponent<ReturnToStartPositionTrigger>();
+			if (trigger != null) {
+				trigger.TriggerEvent -= OnTrigger;
+			}
+		}
+
+		private void OnTrigger() {
+			StartCoroutine(Return(_waitTime,_returnTime));
+		}
+
+		private IEnumerator Return(float delay, float lerpTime) {
+			yield return new WaitForSeconds(delay);
+			_rigidbody.velocity = Vector3.zero;
+			_rigidbody.angularVelocity = Vector3.zero; 
+			
+			float elapsedTime = 0;
+			Vector3 lerpStartPosition = _transform.position;
+			Quaternion lerpStartRotation = _transform.rotation;
+ 
+			while (elapsedTime < lerpTime)
+			{
+				_transform.position = Vector3.Slerp(lerpStartPosition, _startingPosition, (elapsedTime / _returnTime));
+				_transform.rotation = Quaternion.Slerp(lerpStartRotation, _startingRotation, elapsedTime / _returnTime);
+				elapsedTime += Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			}
+			
+		}
 	}
 }
---
+++ Assets/Code/ReturnToStartPositionTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Code {
+	public class ReturnToStartPositionTrigger : MonoBehaviour {
+
+		public delegate void Trigger();
+		public event Trigger TriggerEvent;
+
+		private void OnTriggerEnter(Collider other) {
+			if (TriggerEvent != null) TriggerEvent();
+		}
+	}
+}
